7ade916fc4f254dade0b82b04c7f367c86a445ec
Karim <Karim@Karim-PC>
2014-12-09 16:05:37 +0100

    Signed-off-by: Karim <Karim@Karim-PC>

--- wild/src/main/java/edu/esprit/wild/UsesrBean.java
+++ wild/src/main/java/edu/esprit/wild/UsesrBean.java
@@ -2,9 +2,11 @@ package edu.esprit.wild;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
@@ -15,12 +17,15 @@ public class UsesrBean implements Serializable{
 	
 	
 	private List<String> users;
+	private Date date;
+	
 	
 	public UsesrBean() {
 	}
 	
 	@PostConstruct
 	public void init(){
+		System.out.println("bean created : " + new Date() );
 		users = new ArrayList<String>();
 		users.add("karim");
 		users.add("zoubeir");
@@ -35,6 +40,15 @@ public class UsesrBean implements Serializable{
 
 	public void setUsers(List<String> users) {
 		this.users = users;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	
+	@PreDestroy
+	public void destroy(){
+		System.out.println("bean destroyed : " + new Date() );
 	}
 	
 	
